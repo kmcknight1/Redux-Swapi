bbde2e07dc30d589ecd28bb2317d2c39067a44d5
kmcknight1 <katiemcknight1991@gmail.com>
2019-06-19 17:46:24 -0500

    middleware added to store

--- src/index.js
+++ src/index.js
@@ -3,17 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
 );
 
 ReactDOM.render(
