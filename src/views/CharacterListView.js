61b7e7ce1c21e9a6de1e9997e652cb122d4c6f12
kmcknight1 <katiemcknight1991@gmail.com>
2019-06-20 00:27:02 -0500

    loading data state fixed

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -19,7 +19,7 @@ class CharacterListView extends React.Component {
   render() {
     if (this.props.isLoading) {
       // return something here to indicate that you are fetching data
-      <h2>Loading Star Wars Characters...</h2>;
+      return <h2>Loading Star Wars Characters...</h2>;
     }
     return (
       <div className="CharactersList_wrapper">
